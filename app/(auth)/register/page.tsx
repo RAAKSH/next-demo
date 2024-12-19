3981b94621aa5896b5e1be8038ed37f97e979fd9
Raksha <rakshac29@gmail.com>
2024-12-19 16:54:27 +0530

    Topics covered Group routes, private routes , pages, layout intro

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Regsiter() {
+    return (
+     <div>Regsiter !</div>
+    );
+  }
+  
---
+++ app/_lib/format-date.tsx
@@ -0,0 +1,8 @@
+
+
+export default function FromatDate() {
+    return (
+     <>Format Date</>
+    );
+  }
+  
